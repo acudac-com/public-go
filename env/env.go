a21b7f42eeb3ad83eeabb90b6efc183f55e81d7d
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-06-14 22:30:58 +0200

    feat(env): Required and Optional env helpers

--- env/env.go
+++ env/env.go
@@ -7,37 +7,54 @@ import (
 	"go.alis.build/alog"
 )
 
-type environment string
-
-const (
-	local environment = "local"
-)
-
 var (
-	Env     = loadEnvironment()
-	Product = loadProduct()
+	Env     = RequiredString("ENV")
+	Product = RequiredString("PRODUCT")
 )
 
 func IsLocal() bool {
-	return Env == local
+	return Env == "local"
 }
 
-// reads and validates ENV environment variable.
-func loadEnvironment() environment {
-	key := "ENV"
-	value := environment(os.Getenv(key))
-	if value == "" {
-		alog.Fatalf(context.Background(), "%s environment variable is required", key)
-	}
-	return value
-}
-
-// reads and validates ENV environment variable.
-func loadProduct() string {
-	key := "PRODUCT"
+func RequiredString(key string) string {
 	value := os.Getenv(key)
 	if value == "" {
 		alog.Fatalf(context.Background(), "%s environment variable is required", key)
 	}
 	return value
+}
+
+func OptionalString(key string, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		value = fallback
+	}
+	return value
+}
+
+func RequiredBool(key string) bool {
+	value := os.Getenv(key)
+	if value == "" {
+		alog.Fatalf(context.Background(), "%s environment variable is required", key)
+	}
+	if value == "true" {
+		return true
+	} else if value == "false" {
+		return false
+	} else {
+		alog.Fatalf(context.Background(), "%s environment variable must be 'true' or 'false'", key)
+		return false // will never execute
+	}
+}
+
+func OptionalBool(key string, fallback bool) bool {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	if value == "true" {
+		return true
+	} else {
+		return false
+	}
 }
