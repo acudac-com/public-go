4b9482ddee1d90c074eae325d4a44a93c00b440e
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-06-29 11:10:10 +0200

    clear sqlx tests

--- sqlx/db_test.go
+++ sqlx/db_test.go
@@ -1,13 +1,1 @@
 package sqlx_test
-
-import (
-	"testing"
-
-	"github.com/acudac-com/public-go/sqlx"
-)
-
-func TestExec(t *testing.T) {
-	if _, err := sqlx.Exec(t.Context(), "create schema gm"); err != nil {
-		t.Fatalf("failed to create schema: %v", err)
-	}
-}
