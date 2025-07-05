8691df435e8ffdfa7dd40194a4e11681d160ec01
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-07-05 15:00:15 +0200

    refactor(mux): middleware -> opts

--- mux/mux.go
+++ mux/mux.go
@@ -6,49 +6,41 @@ import (
 )
 
 type (
-	Mux[CxT context.Context] struct {
+	Mux[CxT context.Context, OptsT any] struct {
 		mux     *http.ServeMux
-		gateway Gateway[CxT]
+		gateway Gateway[CxT, OptsT]
 	}
-	Handler[CxT context.Context] func(cx CxT, w http.ResponseWriter, r *http.Request) error
-	Gateway[CxT context.Context] func(w http.ResponseWriter, r *http.Request, handler Handler[CxT], mw ...*Middleware[CxT]) error
+	Handler[CxT context.Context, OptsT any] func(cx CxT, w http.ResponseWriter, r *http.Request, opts OptsT) error
+	Gateway[CxT context.Context, OptsT any] func(w http.ResponseWriter, r *http.Request, handler Handler[CxT, OptsT], opts OptsT) error
 )
 
-// Runs just before and just after a main handler to insert common handling logic.
-type Middleware[CxT context.Context] struct {
-	// Optional handler to call before the main handler starts
-	Prep Handler[CxT]
-	// Optional handler to call after the main handler finished
-	Process Handler[CxT]
+func New[CxT context.Context, OptsT any](gateway Gateway[CxT, OptsT]) *Mux[CxT, OptsT] {
+	return &Mux[CxT, OptsT]{http.NewServeMux(), gateway}
 }
 
-func New[CxT context.Context](gateway Gateway[CxT]) *Mux[CxT] {
-	return &Mux[CxT]{http.NewServeMux(), gateway}
+func (m *Mux[CxT, OptsT]) Get(pattern string, handleFunc Handler[CxT, OptsT], opts OptsT) {
+	m.mux.HandleFunc("GET "+pattern, m.handler(handleFunc, opts))
 }
 
-func (m *Mux[CxT]) Get(pattern string, handleFunc Handler[CxT], middleware ...*Middleware[CxT]) {
-	m.mux.HandleFunc("GET "+pattern, m.handler(handleFunc, middleware))
+func (m *Mux[CxT, OptsT]) Post(pattern string, handleFunc Handler[CxT, OptsT], opts OptsT) {
+	m.mux.HandleFunc("POST "+pattern, m.handler(handleFunc, opts))
 }
 
-func (m *Mux[CxT]) Post(pattern string, handleFunc Handler[CxT], middleware ...*Middleware[CxT]) {
-	m.mux.HandleFunc("POST "+pattern, m.handler(handleFunc, middleware))
+func (m *Mux[CxT, OptsT]) Patch(pattern string, handleFunc Handler[CxT, OptsT], opts OptsT) {
+	m.mux.HandleFunc("PATCH "+pattern, m.handler(handleFunc, opts))
 }
 
-func (m *Mux[CxT]) Patch(pattern string, handleFunc Handler[CxT], middleware ...*Middleware[CxT]) {
-	m.mux.HandleFunc("PATCH "+pattern, m.handler(handleFunc, middleware))
+func (m *Mux[CxT, OptsT]) Put(pattern string, handleFunc Handler[CxT, OptsT], opts OptsT) {
+	m.mux.HandleFunc("PUT "+pattern, m.handler(handleFunc, opts))
 }
 
-func (m *Mux[CxT]) Put(pattern string, handleFunc Handler[CxT], middleware ...*Middleware[CxT]) {
-	m.mux.HandleFunc("PUT "+pattern, m.handler(handleFunc, middleware))
+func (m *Mux[CxT, OptsT]) Delete(pattern string, handleFunc Handler[CxT, OptsT], opts OptsT) {
+	m.mux.HandleFunc("DELETE "+pattern, m.handler(handleFunc, opts))
 }
 
-func (m *Mux[CxT]) Delete(pattern string, handleFunc Handler[CxT], middleware ...*Middleware[CxT]) {
-	m.mux.HandleFunc("DELETE "+pattern, m.handler(handleFunc, middleware))
-}
-
-func (m *Mux[CxT]) handler(handleFunc Handler[CxT], middleware []*Middleware[CxT]) func(w http.ResponseWriter, r *http.Request) {
+func (m *Mux[CxT, OptsT]) handler(handleFunc Handler[CxT, OptsT], opts OptsT) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if err := m.gateway(w, r, handleFunc, middleware...); err != nil {
+		if err := m.gateway(w, r, handleFunc, opts); err != nil {
 			if httpErr, ok := err.(*Error); ok {
 				http.Error(w, httpErr.msg, httpErr.code)
 			} else {
@@ -58,6 +50,6 @@ func (m *Mux[CxT]) handler(handleFunc Handler[CxT], middleware []*Middleware[CxT
 	}
 }
 
-func (m *Mux[CxT]) ListenAndServe(addr string) error {
+func (m *Mux[CxT, OptsT]) ListenAndServe(addr string) error {
 	return http.ListenAndServe(addr, m.mux)
 }
